c188eb37733e42c0356294bd87038a858bfe724c
aidcol <aidancolon20@gmail.com>
2025-02-23 21:14:31 -0600

    Create venv using poetry

---
+++ bumblebee/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import torch
+import musiclang_predict
+
+if __name__ == '__main__':
+    print(f'{np.__version__=}')
+    print(f'{tf.__version__=}')
+    print(f'{torch.__version__=}')
+    print('Hello, world!')
